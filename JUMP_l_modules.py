7dd2fa5c69ffc40ac472d268c16302e562eb923a
Suresh Poudel <suresh.poudel@stjude.org>
2021-02-16 12:45:52 -0600

    Ion series function modified for theretical ions computation; symbol dictionary added based on automated pep.xml file parsing to get modificaiton dicationary

--- JUMP_l_modules.py
+++ JUMP_l_modules.py
@@ -109,6 +109,8 @@ def mkdir(dir1):
   except:
     "Dynamic Modification Directory exits!!"
 
+
+
 def createSymbolDict(posMassDict):
   seqSymbol = {}
   for vals in posMassDict.values():
@@ -121,6 +123,25 @@ def createSymbolDict(posMassDict):
     else:
       newVal = "("+str(vals)+")"
       seqSymbol[newVal]  = "*"
+  return seqSymbol
+
+
+
+def createSymbolDict2(posMassDict, jump_mod_dict, sta_AA):
+  seqSymbol = {}
+  for keys, vals in posMassDict.items():
+    for keyJ, valsJ in jump_mod_dict.items():
+        if vals == valsJ:
+            seqKey = "("+str(vals)+")"
+            seqVal = keyJ
+            seqSymbol[seqKey] = seqVal
+    
+    for keySta, valsSta in sta_AA.items():
+        if vals == valsSta:
+            seqKey = "("+str(vals)+")"
+            seqVal = "+"+str(int(valsSta))
+            seqSymbol[seqKey] = seqVal
+    
   return seqSymbol
 
 
@@ -187,13 +208,13 @@ def modsForReport(mods, peptide):
   return modsPrint
 
 
-def reformat_dataframe2(df, posMassDict, match_list):
+def reformat_dataframe2(df, posMassDict,jump_mod_dict, sta_AA, match_list):
   matched_list_array = list(np.array(match_list).round(4))
   dfNew = df.copy() #duplicate dataframe to work
 
   df2 = dfNew.rename(columns={"Peptide_Mod_Seq":"Seq"})
   
-  seqSymbol = createSymbolDict(posMassDict)
+  seqSymbol = createSymbolDict2(posMassDict,jump_mod_dict, sta_AA)
 
   matched_list_array = list(np.array(match_list).round(4))
 
@@ -211,9 +232,9 @@ def reformat_dataframe2(df, posMassDict, match_list):
   return df2, seqSymbol
 
 
-def excelWriter2(writer, df,  worksheetName, figure1, figure2,spectrumSplit,xcorr, prob,lscoreSite, massSeriesLength, matched, seqSymbol, peptide, N=34, updater=0):
+def excelWriter2(writer, df,  worksheetName, figure1, figure2,spectrumSplit,xcorr, prob,lscoreSite, massSeriesLength, matched, seqSymbol,jump_mod_dict, sta_AA, peptide, N=34, updater=0):
   text1 = "Spectrum = "+spectrumSplit[0]+"; Plain Peptide Sequence = "+spectrumSplit[1]+"; Modifications on the Peptide = "+modsForReport(spectrumSplit[2], peptide)
-  text2 = "Xcorr = "+str(xcorr)+"; Localization Probablity Score = "+ str(prob)+"; Localization Site:Score = "+ lscoreSite
+  text2 = "Xcorr = "+xcorr.astype("str")+"; Localization Probablity Score = "+ str(prob)+"; Localization Site:Score = "+ lscoreSite
   text3 = "Figure a) Intensity Plot"
   text4 = "Figure b) Mass deviation of matched ions (ppm tolerance) Plot"
   text5 = "Figure c) Ion series b and y ions with different charge states Plot" 
@@ -507,23 +528,39 @@ def valAddKey(dict1, key, val):
 # In[6]:
 
 
+def getPhoPosition(massPosDict):
+    phoPos = []
+    for key, value in massPosDict.items():
+        if "79.96" in str(value):
+            phoPos.append(int(key))
+    return sorted(phoPos)
+            
+
 def ionSeriesIonLossSpeRes(peptide,massPosDict, maxcharge=1,useMod ="Yes"):
-  if useMod != "Yes":
-    massPosDict = {0:0.0}
-  h2o = mass.calculate_mass(formula='H2O')
-  co = mass.calculate_mass(formula='CO')
-  nh3 = mass.calculate_mass(formula='NH3')
-  xmassMore = co-mass.calculate_mass(formula='H2')
-  proton = mass.calculate_mass(formula='H+')
-  
-  hpo3 = mass.calculate_mass(formula='HPO3')
-  h3po4 = mass.calculate_mass(formula='H3PO4')
+  #first check if the mods has phosphorylation if yes get the maximum and minimum position
+  phoPos = getPhoPosition(massPosDict)
+  minPos = 1  #assigns minPosition which is later updated
+  maxPos = len(peptide) #assigns maximum position which is later updated
+  if len(phoPos) >= 1:
+    minPos = np.min(phoPos) #computes minimum phospho position so tha b and y ions can be computed according for phospho loss
+    maxPos = np.max(phoPos) #computes maximum phospho position so that b and y ions can be computed according ly
 
-  all_ions_dict = {}
+  if useMod != "Yes": #this checks whether the modificaiton is searched or not, generally this is always Yes for localization 
+    massPosDict = {0:0.0} #if modificaiton is no than the dictionary has no information
+  h2o = mass.calculate_mass(formula='H2O') #h2o mono mass
+  co = mass.calculate_mass(formula='CO') #co mono mass
+  nh3 = mass.calculate_mass(formula='NH3') #nh3 mono mass
+  xmassMore = co-mass.calculate_mass(formula='H2') #hydrogen mono mass
+  proton = mass.calculate_mass(formula='H+') #proron mono mass
+  hydrogenAtom = mass.calculate_mass(formula='H') #hydrogen atom mono mass
+  hpo3 = mass.calculate_mass(formula='HPO3') #computes hpo3 mono mass
+  h3po4 = mass.calculate_mass(formula='H3PO4') #computes h3po4 mono mass
 
+  all_ions_dict = {}#iniitates a dictionary for theoretical ions which is later conveted to dataframe
+#possible ion loss or no ion loss
   ionloss = {"":0,"-H2O":h2o,"-HPO3":hpo3,"-H3PO4":h3po4,"-NH3":nh3}
   
-  
+#this section computes a,b,c ions  
   for i in range(1, len(peptide)+1):
     addModMass = 0.0
     for massKey in massPosDict.keys():
@@ -534,16 +571,18 @@ def ionSeriesIonLossSpeRes(peptide,massPosDict, maxcharge=1,useMod ="Yes"):
 #     print (peptide[0:i-1])
     for losses in ionloss.keys():
       if losses == "-H2O":
+#water loss if the amino acid sequence is STED
         fate = checkAA(peptide[0:i],["S","T","E","D"])
         for charge in range(1, maxcharge+1):
-          if fate == "True":
+          if (fate == "True") and (i < len(peptide)):
             valAddKey(all_ions_dict,"b"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o-ionloss[losses]+addModMass)/charge)
           else:
             valAddKey(all_ions_dict,"b"+losses+"+"+str(charge),"---")
       if losses == "-NH3":
+#ammonia loss if the amono acid sequence is RKQN
         fate = checkAA(peptide[0:i],["R","K","Q","N"])
         for charge in range(1, maxcharge+1):
-          if fate == "True":
+          if (fate == "True") and (i < len(peptide)):
             valAddKey(all_ions_dict,"b"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o-ionloss[losses]+addModMass)/charge)
             valAddKey(all_ions_dict,"a"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o-co-ionloss[losses]+addModMass)/charge)
           else:
@@ -553,8 +592,10 @@ def ionSeriesIonLossSpeRes(peptide,massPosDict, maxcharge=1,useMod ="Yes"):
       if (losses == "-H3PO4") or (losses == "-HPO3"):
         for charge in range(1, maxcharge+1):
           if "79.96" in str(massPosDict.values()):
-            fate = checkAA(peptide[0:i],["S","T","Y"])
-            if fate == "True":
+#             fate = checkAA(peptide[0:i],["S","T","Y"])
+#             if fate == "True":
+#phosoho loss if massPosDict have phopsho loss in it. If yes based on minPos and maxPos ions are calculated
+            if (i >= minPos) and (i < len(peptide)):
               valAddKey(all_ions_dict,"b"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o-ionloss[losses]+addModMass)/charge)
               valAddKey(all_ions_dict,"a"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o-co-ionloss[losses]+addModMass)/charge)
             else:
@@ -563,11 +604,23 @@ def ionSeriesIonLossSpeRes(peptide,massPosDict, maxcharge=1,useMod ="Yes"):
                       
       if losses == "":
         for charge in range(1, maxcharge+1):
-          valAddKey(all_ions_dict,"b"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o-ionloss[losses]+addModMass)/charge)
-          valAddKey(all_ions_dict,"a"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o-co-ionloss[losses]+addModMass)/charge)
-          valAddKey(all_ions_dict,"c"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o+nh3-ionloss[losses]+addModMass)/charge)
-         
+          if i < len(peptide):
+            valAddKey(all_ions_dict,"b"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o-ionloss[losses]+addModMass)/charge)
+            valAddKey(all_ions_dict,"a"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o-co-ionloss[losses]+addModMass)/charge)
+            valAddKey(all_ions_dict,"c"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o+nh3-ionloss[losses]+addModMass)/charge)
+            valAddKey(all_ions_dict,"c(-1)"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o+nh3-ionloss[losses]+addModMass)/charge-hydrogenAtom)
+            valAddKey(all_ions_dict,"c1"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o+nh3-ionloss[losses]+addModMass)/charge+hydrogenAtom)
+            valAddKey(all_ions_dict,"c2"+losses+"+"+str(charge),(mass.calculate_mass(peptide[:i])+(charge*proton)-h2o+nh3-ionloss[losses]+addModMass)/charge+(2*hydrogenAtom))
 
+          else:
+            valAddKey(all_ions_dict,"b"+losses+"+"+str(charge),"---")
+            valAddKey(all_ions_dict,"a"+losses+"+"+str(charge),"---")
+            valAddKey(all_ions_dict,"c"+losses+"+"+str(charge),"---")
+            valAddKey(all_ions_dict,"c(-1)"+losses+"+"+str(charge),"---")
+            valAddKey(all_ions_dict,"c1"+losses+"+"+str(charge),"---")
+            valAddKey(all_ions_dict,"c2"+losses+"+"+str(charge),"---")
+
+#this section computes x,y,z ions
   for i in range(0, len(peptide)):
     
     addModMass = 0.0
@@ -578,7 +631,7 @@ def ionSeriesIonLossSpeRes(peptide,massPosDict, maxcharge=1,useMod ="Yes"):
       if losses == "-H2O":
         fate = checkAA(peptide[i:len(peptide)],["S","T","E","D"])
         for charge in range(1, maxcharge+1):
-          if fate == "True":
+          if (fate == "True") and (i >=1):
             valAddKey(all_ions_dict,"y"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-ionloss[losses]+addModMass)/charge)
           else:
             valAddKey(all_ions_dict,"y"+losses+"+"+str(charge),"---")
@@ -586,33 +639,45 @@ def ionSeriesIonLossSpeRes(peptide,massPosDict, maxcharge=1,useMod ="Yes"):
       if losses == "-NH3":
         fate = checkAA(peptide[i:len(peptide)],["R","K","Q","N"])
         for charge in range(1, maxcharge+1):
-          if fate == "True":
+          if (fate == "True") and (i >=1):
             valAddKey(all_ions_dict,"y"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-ionloss[losses]+addModMass)/charge)
           else:
             valAddKey(all_ions_dict,"y"+losses+"+"+str(charge),"---")
       
       
       if (losses == "-H3PO4") or (losses == "-HPO3"):
-        fate = checkAA(peptide[i:len(peptide)],["S","T","Y"])
+#         fate = checkAA(peptide[i:len(peptide)],["S","T","Y"])
         for charge in range(1, maxcharge+1):
           if "79.96" in str(massPosDict.values()):
-            if fate == "True":
+            if (i < maxPos) and (i >=1):
               valAddKey(all_ions_dict,"y"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-ionloss[losses]+addModMass)/charge)
             else:
               valAddKey(all_ions_dict,"y"+losses+"+"+str(charge),"---")
       if losses == "":
         for charge in range(1, maxcharge+1):
-          valAddKey(all_ions_dict,"y"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-ionloss[losses]+addModMass)/charge)
-          valAddKey(all_ions_dict,"x"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)+xmassMore-ionloss[losses]+addModMass)/charge)
-          valAddKey(all_ions_dict,"z"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-nh3-ionloss[losses]+addModMass)/charge)
-      
-    
+          if (i >=1):
+            valAddKey(all_ions_dict,"y"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-ionloss[losses]+addModMass)/charge)
+            valAddKey(all_ions_dict,"x"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)+xmassMore-ionloss[losses]+addModMass)/charge)
+
+            valAddKey(all_ions_dict,"z"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-nh3-ionloss[losses]+addModMass)/charge+hydrogenAtom)
+            valAddKey(all_ions_dict,"z1"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-nh3-ionloss[losses]+addModMass)/charge+(2*hydrogenAtom))
+            valAddKey(all_ions_dict,"z2"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-nh3-ionloss[losses]+addModMass)/charge+(3*hydrogenAtom))
+            valAddKey(all_ions_dict,"z3"+losses+"+"+str(charge),(mass.calculate_mass(peptide[i:])+(charge*proton)-nh3-ionloss[losses]+addModMass)/charge+(4*hydrogenAtom))
+          else:
+            valAddKey(all_ions_dict,"y"+losses+"+"+str(charge),"---")
+            valAddKey(all_ions_dict,"x"+losses+"+"+str(charge),"---")
+
+            valAddKey(all_ions_dict,"z"+losses+"+"+str(charge),"---")
+            valAddKey(all_ions_dict,"z1"+losses+"+"+str(charge),"---")
+            valAddKey(all_ions_dict,"z2"+losses+"+"+str(charge),"---")
+            valAddKey(all_ions_dict,"z3"+losses+"+"+str(charge),"---")
+         
                 
 
   df = pd.DataFrame(all_ions_dict)
   return df
-
-
+#for z ion calculation please see the notes:
+#https://prospector.ucsf.edu/prospector/html/misc/faq.htm#z_ions
 # In[5]:
 def ionMatches(exp_mz_list, intensity_list, theor_fragments_df, ion_types = ["b","y"], ionLoss = ["NH3","H2O"], tol=10):
     
